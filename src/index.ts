50bb7885e60f5b659b5846782b52cb401b4cfb25
Thang Nguyen <thangnv77@gmail.com>
2017-08-10 15:29:26 +0700

    Add exported decorators/methods to index file

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export { Field, Arg, Context, Info } from './decorators/field'
+
+export { ObjectType } from './decorators/objectType'
+
+export { Description } from './decorators/description'
+
+export { Query, Mutation, buildGraphQLSchema } from './decorators/schema'
