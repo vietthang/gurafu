d61e1381ef29291e6f9c952d822d1658aeaa43a7
Thang Nguyen <thangnv77@gmail.com>
2017-08-11 14:54:30 +0700

    Refactor objectTypeFactor to using once function

--- src/factory/objectTypeFactory.ts
+++ src/factory/objectTypeFactory.ts
@@ -6,53 +6,32 @@ import {
 import { getObjectTypeName } from '../decorators/objectType'
 import { getDescription } from '../decorators/description'
 import { fieldsFactory, inputFieldsFactory } from './fieldsFactory'
+import { once, Callable1 } from '../utils'
 
-const cachedObjectType = Symbol('cachedObjectType')
-
-const cachedInputObjectType = Symbol('cachedInputObjectType')
-
-export function objectTypeFactory(target: Function): GraphQLObjectType {
-  let objectType = Reflect.getMetadata(cachedObjectType, target.prototype)
-  if (objectType) {
-    return objectType
-  }
-
+export const objectTypeFactory: Callable1<Function,GraphQLObjectType> = once((target) => {
   const description = getDescription(target)
   const name = getObjectTypeName(target)
   if (!name) {
     throw new Error(`Missing @ObjectType in ${target.name}`)
   }
 
-  objectType = new GraphQLObjectType({
+  return new GraphQLObjectType({
     name,
     description,
     fields: fieldsFactory(target),
   })
+})
 
-  Reflect.defineMetadata(cachedObjectType, objectType, target.prototype)
-
-  return objectType
-}
-
-export function inputObjectTypeFactory(target: Function): GraphQLInputObjectType {
-  let inputObjectType = Reflect.getMetadata(cachedInputObjectType, target.prototype)
-  if (inputObjectType) {
-    return inputObjectType
-  }
-
+export const inputObjectTypeFactory: Callable1<Function,GraphQLInputObjectType> = once((target) => {
   const description = getDescription(target)
   const name = getObjectTypeName(target)
   if (!name) {
     throw new Error(`Missing @ObjectType in ${target.name}`)
   }
 
-  inputObjectType = new GraphQLInputObjectType({
+  return new GraphQLInputObjectType({
     name: name + 'Input',
     description,
     fields: inputFieldsFactory(target),
   })
-
-  Reflect.defineMetadata(cachedInputObjectType, inputObjectType, target.prototype)
-
-  return inputObjectType
-}
+})
