d8bd86d04e744c33e5bddc9deb549691ecf08790
Thang Nguyen <thangnv77@gmail.com>
2017-08-22 13:45:03 +0700

    Fix this binding for schema

--- src/decorators/schema.ts
+++ src/decorators/schema.ts
@@ -4,36 +4,57 @@ import { Field } from './field'
 
 const schemaMethodKindSymbol = Symbol('schemaMethodKind')
 
-export function Mutation(name?: string): PropertyDecorator {
-  return (target, key) => {
+export function Mutation(name?: string): Function {
+  return (target: any, key: string) => {
     const oldType = Reflect.getMetadata(schemaMethodKindSymbol, target, key)
     if (oldType) {
       throw new Error(`Key ${key} has been decorated with ${oldType} already`)
     }
     Field(name)(target, key)
     Reflect.defineMetadata(schemaMethodKindSymbol, '@Mutation', target, key)
+    if (target[key] && typeof target[key] === 'function') {
+      return {
+        value: target[key].bind(target),
+      }
+    }
+
+    return undefined
   }
 }
 
-export function Query(name?: string): PropertyDecorator {
-  return (target, key) => {
+export function Query(name?: string): Function {
+  return (target: any, key: string) => {
     const oldType = Reflect.getMetadata(schemaMethodKindSymbol, target, key)
     if (oldType) {
       throw new Error(`Key ${key} has been decorated with ${oldType} already`)
     }
     Field(name)(target, key)
     Reflect.defineMetadata(schemaMethodKindSymbol, '@Query', target, key)
+    if (target[key] && typeof target[key] === 'function') {
+      return {
+        value: target[key].bind(target),
+      }
+    }
+
+    return undefined
   }
 }
 
-export function Subscription(name?: string): PropertyDecorator {
-  return (target, key) => {
+export function Subscription(name?: string): Function {
+  return (target: any, key: string) => {
     const oldType = Reflect.getMetadata(schemaMethodKindSymbol, target, key)
     if (oldType) {
       throw new Error(`Key ${key} has been decorated with ${oldType} already`)
     }
     Field(name)(target, key)
     Reflect.defineMetadata(schemaMethodKindSymbol, '@Subscription', target, key)
+    if (target[key] && typeof target[key] === 'function') {
+      return {
+        value: target[key].bind(target),
+      }
+    }
+
+    return undefined
   }
 }
 
